d1feadd320a815d1ec3d043225397bf375c322b3
Ark Software Technologies <arksoftwaretechnologies@gmail.com>
2024-08-16 19:05:44 +0530

    Out folder changes

--- src/app/api/easun/Login/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { cookies,  } from "next/headers";
-
-export async function GET(){
-  cookies().delete("user");
-  return NextResponse.json({ message: "OK" }, { status: 200 });
-}
-
