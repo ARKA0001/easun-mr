6800315e5963a5f6260ef65a43d9b1f8f9613525
Ranganath <ransnbvr@gmail.com>
2024-09-26 22:02:28 +0530

    DESC: Button name changed

--- src/components/modals/InfoModal.jsx
+++ src/components/modals/InfoModal.jsx
@@ -120,7 +120,7 @@ export default function InfoModal({ showModal, modalMessage, messageAction }) {
                   onClick={() => messageAction("DONE")}
                   className="resume-button"
                 >
-                  Download Report
+                  OK
                 </button>
               </>
             )}
